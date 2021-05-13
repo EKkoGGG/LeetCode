856ffdc7393fb3c91c76a72cd5da27bb23bddd1a
曾伟杰 <330564052@qq.com>
2021-05-13 16:49:10 +0800

    0021

--- src/21.合并两个有序链表.cs
+++ src/21.合并两个有序链表.cs
@@ -18,20 +18,44 @@
  */
 public class Solution
 {
+    // 递归
     public ListNode MergeTwoLists(ListNode l1, ListNode l2)
     {
-        if(l1 == null) return l2;
+        if (l1 == null) return l2;
         else if (l2 == null) return l1;
         else if (l1.val < l2.val)
         {
-            l1.next = MergeTwoLists(l1.next,l2);
+            l1.next = MergeTwoLists(l1.next, l2);
             return l1;
         }
         else
         {
-            l2.next = MergeTwoLists(l1,l2.next);
+            l2.next = MergeTwoLists(l2.next, l1);
             return l2;
         }
+    }
+
+    // 迭代
+    public ListNode MergeTwoLists(ListNode l1, ListNode l2)
+    {
+        var prehead = new ListNode(-1);
+        var prev = prehead;
+        while(l1 != null && l2 != null)
+        {
+            if(l1.val <= l2.val)
+            {
+                prev.next = l1;
+                l1 = l1.next;
+            }
+            else
+            {
+                prev.next = l2;
+                l2 = l2.next;
+            }
+            prev = prev.next;
+        }
+        prev.next = l1 == null ? l2 : l1;
+        return prehead.next;
     }
 }
 // @lc code=end
