61cefdda331c0d0ea0e288aac69840cb0285a4c3
ZWJ <330564052@qq.com>
2021-05-17 23:05:31 +0800

    0088

--- src/88.合并两个有序数组.cs
+++ src/88.合并两个有序数组.cs
@@ -7,11 +7,46 @@
 // @lc code=start
 public class Solution
 {
+    // 双指针
+    public void Merge(int[] nums1, int m, int[] nums2, int n)
+    {
+        int p1 = m - 1;
+        int p2 = n - 1;
+        int tail = m + n - 1;
+        int cur;
+        while (p1 >= 0 || p2 >= 0)
+        {
+            if (p1 < 0)
+            {
+                cur = nums2[p2];
+                p2--;
+            }
+            else if (p2 < 0)
+            {
+                cur = nums1[p1];
+                p1--;
+            }
+            else if (nums1[p1] > nums2[p2])
+            {
+                cur = nums1[p1];
+                p1--;
+            }
+            else
+            {
+                cur = nums2[p2];
+                p2--;
+            }
+            nums1[tail] = cur;
+            tail--;
+        }
+    }
+
+    // 自己实现
     public void Merge(int[] nums1, int m, int[] nums2, int n)
     {
         int index = 0;
         bool isEnd = false;
-        if(m == 0) isEnd = true;
+        if (m == 0) isEnd = true;
         for (int i = 0; i < n; i++)
         {
             for (int j = index; j < m; j++)
