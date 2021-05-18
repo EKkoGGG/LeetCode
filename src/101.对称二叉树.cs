1f73541fdaf1df34fecee7fe087cc3c5f043dc32
ZWJ <330564052@qq.com>
2021-05-18 23:29:42 +0800

    0101

---
+++ src/101.对称二叉树.cs
@@ -0,0 +1,33 @@
+/*
+ * @lc app=leetcode.cn id=101 lang=csharp
+ *
+ * [101] 对称二叉树
+ */
+
+// @lc code=start
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     public int val;
+ *     public TreeNode left;
+ *     public TreeNode right;
+ *     public TreeNode(int val=0, TreeNode left=null, TreeNode right=null) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+public class Solution
+{
+    public bool IsSymmetric(TreeNode root)
+    {
+        if(root.left == null || root.right == null)
+        {
+            return true;
+        }
+        int leftVal = IsSymmetric
+    }
+}
+// @lc code=end
+
