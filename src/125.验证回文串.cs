3c71d515778f23740b1a17acd9a52aac43ae51da
曾伟杰 <330564052@qq.com>
2021-05-24 14:45:19 +0800

    0125

--- src/125.验证回文串.cs
+++ src/125.验证回文串.cs
@@ -5,11 +5,40 @@
  */
 
 // @lc code=start
+using System.Text.RegularExpressions;
 public class Solution
 {
+    // 迭代
     public bool IsPalindrome(string s)
     {
-
+        s = s.ToUpper();
+        var pattern = "[A-Z0-9]";
+        var endIndex = s.Length - 1;
+        for (int i = 0; i < s.Length; i++)
+        {
+            var startStr = s[i].ToString();
+            if (Regex.IsMatch(startStr, pattern))
+            {
+                for (int j = endIndex; j >= 0; j--)
+                {
+                    var endStr = s[j].ToString();
+                    if (Regex.IsMatch(endStr, pattern))
+                    {
+                        if (startStr == endStr)
+                        {
+                            j--;
+                            endIndex = j;
+                            break;
+                        }
+                        else
+                        {
+                            return false;
+                        }
+                    }
+                }
+            }
+        }
+        return true;
     }
 }
 // @lc code=end
