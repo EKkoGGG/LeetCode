a770666e8b355ae99479ee699e554d1e9cece03b
曾伟杰 <330564052@qq.com>
2021-05-27 14:59:55 +0800

    0160

--- src/160.相交链表.cs
+++ src/160.相交链表.cs
@@ -49,6 +49,72 @@ public class Solution
         }
         return null;
     }
+
+    // 双指针
+    public ListNode GetIntersectionNode(ListNode headA, ListNode headB)
+    {
+        var pA = headA;
+        var pB = headB;
+        var isARep = false;
+        var isBRep = false;
+        while (pA != null || pB != null)
+        {
+            if (pA == pB)
+            {
+                return pA;
+            }
+            if (pA == null && !isARep)
+            {
+                isARep = true;
+                pA = headB;
+            }
+            else if (pA != null)
+            {
+                pA = pA.next;
+            }
+            if (pB == null && !isBRep)
+            {
+                isBRep = true;
+                pB = headA;
+            }
+            else if (pB != null)
+            {
+                pB = pB.next;
+            }
+        }
+        return null;
+    }
+
+    // 双指针
+    public ListNode GetIntersectionNode(ListNode headA, ListNode headB)
+    {
+        if (headA == null || headB == null)
+        {
+            return null;
+        }
+        var pA = headA;
+        var pB = headB;
+        while (pA != pB)
+        {
+            if (pA == null)
+            {
+                pA = headB;
+            }
+            else
+            {
+                pA = pA.next;
+            }
+            if (pB == null)
+            {
+                pB = headA;
+            }
+            else
+            {
+                pB = pB.next;
+            }
+        }
+        return pA;
+    }
 }
 // @lc code=end
 
