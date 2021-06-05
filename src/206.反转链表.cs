4b03d24cb848d7349186e6f9112126cc7dbcabad
曾伟杰 <330564052@qq.com>
2021-06-05 11:03:45 +0800

    0206

---
+++ src/206.反转链表.cs
@@ -0,0 +1,27 @@
+/*
+ * @lc app=leetcode.cn id=206 lang=csharp
+ *
+ * [206] 反转链表
+ */
+
+// @lc code=start
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     public int val;
+ *     public ListNode next;
+ *     public ListNode(int val=0, ListNode next=null) {
+ *         this.val = val;
+ *         this.next = next;
+ *     }
+ * }
+ */
+public class Solution
+{
+    public ListNode ReverseList(ListNode head)
+    {
+
+    }
+}
+// @lc code=end
+
