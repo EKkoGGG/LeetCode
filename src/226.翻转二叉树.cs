cd94d2f701de22546d5f07723e1d283d3ad3b10f
曾伟杰 <330564052@qq.com>
2021-06-07 09:10:22 +0800

    0226

---
+++ src/226.翻转二叉树.cs
@@ -0,0 +1,29 @@
+/*
+ * @lc app=leetcode.cn id=226 lang=csharp
+ *
+ * [226] 翻转二叉树
+ */
+
+// @lc code=start
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     public int val;
+ *     public TreeNode left;
+ *     public TreeNode right;
+ *     public TreeNode(int val=0, TreeNode left=null, TreeNode right=null) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+public class Solution
+{
+    public TreeNode InvertTree(TreeNode root)
+    {
+
+    }
+}
+// @lc code=end
+
