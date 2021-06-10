8889af116898dbfa5fbcf761195ae75eda824a5b
ZWJ <330564052@qq.com>
2021-06-10 22:05:52 +0800

    0263

--- src/263.丑数.cs
+++ src/263.丑数.cs
@@ -9,7 +9,19 @@ public class Solution
 {
     public bool IsUgly(int n)
     {
-
+        if (n <= 0)
+        {
+            return false;
+        }
+        var list = new List<int>(){2,3,5};
+        foreach (var item in list)
+        {
+            while (n % item == 0)
+            {
+                n /= item;
+            }
+        }
+        return n == 1;
     }
 }
 // @lc code=end
