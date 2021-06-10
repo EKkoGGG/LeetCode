cd081aae0901a05a20d7ec5bdef8f0a7c4f1a927
曾伟杰 <330564052@qq.com>
2021-06-10 09:08:33 +0800

    0263

---
+++ src/263.丑数.cs
@@ -0,0 +1,16 @@
+/*
+ * @lc app=leetcode.cn id=263 lang=csharp
+ *
+ * [263] 丑数
+ */
+
+// @lc code=start
+public class Solution
+{
+    public bool IsUgly(int n)
+    {
+
+    }
+}
+// @lc code=end
+
