5aaaf276ce2a2e453ef641a2b12cfb254de02eb3
曾伟杰 <330564052@qq.com>
2021-06-11 11:28:22 +0800

    0283

--- src/283.移动零.cs
+++ src/283.移动零.cs
@@ -10,20 +10,15 @@ public class Solution
     public void MoveZeroes(int[] nums)
     {
         var length = nums.Length;
+        var j = 0;
         for (int i = 0; i < length; i++)
         {
-            if (nums[i] == 0)
+            if (nums[i] != 0)
             {
-                for (int j = i + 1; j < length; j++)
-                {
-                    if (nums[j] != 0)
-                    {
-                        nums[i] = nums[j];
-                        nums[j] = 0;
-                        break;
-                    }
-                }
-                continue;
+                var temp = nums[i];
+                nums[i] = nums[j];
+                nums[j] = temp;
+                j++;                
             }
         }
     }
