f524c99c2556f7250987e4be28ceca7e9fb224bd
ZWJ <330564052@qq.com>
2021-06-20 10:10:11 +0800

    0437

--- src/437.路径总和-iii.cs
+++ src/437.路径总和-iii.cs
@@ -20,10 +20,34 @@
  */
 public class Solution
 {
+    private int count;
     public int PathSum(TreeNode root, int targetSum)
     {
-
+        count = 0;
+        Dfs(root, targetSum, new List<int>());
+        return count;
     }
+
+    private void Dfs(TreeNode root, int sum, IList<int> sumResult)
+    {
+        if (root == null) return;
+
+        var curValue = root.val;
+        for (var i = 0; i < sumResult.Count; i++)
+            sumResult[i] += curValue;
+        sumResult.Add(curValue);
+
+        foreach (var item in sumResult)
+            if (item == sum) count++;
+
+        Dfs(root.left, sum, sumResult);
+        Dfs(root.right, sum, sumResult);
+
+        for (var i = 0; i < sumResult.Count; i++)
+            sumResult[i] -= curValue;
+        sumResult.Remove(0);
+    }
+
 }
 // @lc code=end
 
