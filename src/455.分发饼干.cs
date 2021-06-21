4d7f514bb5f05060a2ac41d9832446598b4398f3
曾伟杰 <330564052@qq.com>
2021-06-21 10:00:05 +0800

    0455

--- src/455.分发饼干.cs
+++ src/455.分发饼干.cs
@@ -5,11 +5,28 @@
  */
 
 // @lc code=start
+using System;
 public class Solution
 {
     public int FindContentChildren(int[] g, int[] s)
     {
-
+        var res = 0;
+        Array.Sort(g);
+        Array.Sort(s);
+        var gIndex = 0;
+        for (int i = 0; i < s.Length; i++)
+        {
+            for (int j = gIndex; j < g.Length; j++)
+            {
+                if (s[i] >= g[j])
+                {
+                    res++;
+                    gIndex++;
+                    break;
+                }
+            }
+        }
+        return res;
     }
 }
 // @lc code=end
