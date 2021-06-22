bb903f60de2ef7bebcae7bdd2cc4b7cc173fef4d
曾伟杰 <330564052@qq.com>
2021-06-22 09:52:41 +0800

    0504

--- src/504.七进制数.cs
+++ src/504.七进制数.cs
@@ -5,9 +5,29 @@
  */
 
 // @lc code=start
-public class Solution {
-    public string ConvertToBase7(int num) {
-
+using System.Collections.Generic;
+public class Solution
+{
+    public string ConvertToBase7(int num)
+    {
+        var res = string.Empty;
+        var remainders = new List<int>();
+        if (num < 0)
+        {
+            num = -num;
+            res = "-";
+        }
+        while (num >= 7)
+        {
+            remainders.Add(num % 7);
+            num /= 7;
+        }
+        remainders.Add(num);
+        for (int i = remainders.Count - 1; i >= 0; i--)
+        {
+            res += remainders[i];
+        }
+        return res;
     }
 }
 // @lc code=end
