aef06b26b2e30c36631b49bfd454734efea560aa
曾伟杰 <330564052@qq.com>
2021-06-22 09:13:06 +0800

    0504

---
+++ src/504.七进制数.cs
@@ -0,0 +1,14 @@
+/*
+ * @lc app=leetcode.cn id=504 lang=csharp
+ *
+ * [504] 七进制数
+ */
+
+// @lc code=start
+public class Solution {
+    public string ConvertToBase7(int num) {
+
+    }
+}
+// @lc code=end
+
