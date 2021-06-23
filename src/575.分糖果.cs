e0ed3f6812fb0bdf8b0fb639e75f291f7a600236
曾伟杰 <330564052@qq.com>
2021-06-23 09:39:10 +0800

    0575

---
+++ src/575.分糖果.cs
@@ -0,0 +1,16 @@
+/*
+ * @lc app=leetcode.cn id=575 lang=csharp
+ *
+ * [575] 分糖果
+ */
+
+// @lc code=start
+public class Solution
+{
+    public int DistributeCandies(int[] candyType)
+    {
+
+    }
+}
+// @lc code=end
+
