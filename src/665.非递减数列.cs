84c955d40c496eca9b2d067af6e0225816f17760
曾伟杰 <330564052@qq.com>
2021-06-25 09:17:27 +0800

    0665

---
+++ src/665.非递减数列.cs
@@ -0,0 +1,16 @@
+/*
+ * @lc app=leetcode.cn id=665 lang=csharp
+ *
+ * [665] 非递减数列
+ */
+
+// @lc code=start
+public class Solution
+{
+    public bool CheckPossibility(int[] nums)
+    {
+
+    }
+}
+// @lc code=end
+
