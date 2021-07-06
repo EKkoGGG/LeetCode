d48cf59b616b92550048f8563f12cdb663742ddc
曾伟杰 <330564052@qq.com>
2021-07-06 14:05:51 +0800

    0821

--- src/821.字符的最短距离.cs
+++ src/821.字符的最短距离.cs
@@ -5,12 +5,34 @@
  */
 
 // @lc code=start
+using System.Collections.Generic;
 public class Solution
 {
     public int[] ShortestToChar(string s, char c)
     {
-        loveleetcode
-        aaaaeaaaaeaa
+        int n = s.Length;
+        var ans = new int[n];
+        var prev = int.MinValue / 2;
+
+        for (int i = 0; i < n; i++)
+        {
+            if ((char)s[i] == c)
+            {
+                prev = i;
+            }
+            ans[i] = i - prev;
+        }
+
+        prev = int.MaxValue / 2;
+        for (int i = n - 1; i >= 0; i--)
+        {
+            if ((char)s[i] == c)
+            {
+                prev = i;
+            }
+            ans[i] = Math.Min(ans[i], prev - i);
+        }
+        return ans;
     }
 }
 // @lc code=end
