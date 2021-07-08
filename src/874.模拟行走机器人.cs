ca782347c7a1d34c803c20790bdab5fdec8418ae
ZWJ <330564052@qq.com>
2021-07-08 23:24:00 +0800

    0874

---
+++ src/874.模拟行走机器人.cs
@@ -0,0 +1,14 @@
+/*
+ * @lc app=leetcode.cn id=874 lang=csharp
+ *
+ * [874] 模拟行走机器人
+ */
+
+// @lc code=start
+public class Solution {
+    public int RobotSim(int[] commands, int[][] obstacles) {
+
+    }
+}
+// @lc code=end
+
