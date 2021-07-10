bf02d1e8cba997d071b7ab58db6564f2fdce9551
ZWJ <330564052@qq.com>
2021-07-10 09:55:08 +0800

    1128

---
+++ src/1128.等价多米诺骨牌对的数量.cs
@@ -0,0 +1,16 @@
+/*
+ * @lc app=leetcode.cn id=1128 lang=csharp
+ *
+ * [1128] 等价多米诺骨牌对的数量
+ */
+
+// @lc code=start
+public class Solution
+{
+    public int NumEquivDominoPairs(int[][] dominoes)
+    {
+
+    }
+}
+// @lc code=end
+
