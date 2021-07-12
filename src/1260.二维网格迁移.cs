fe5779e0e2ebbfe7d2659a626096d40c1d36b19d
ZWJ <330564052@qq.com>
2021-07-12 22:46:04 +0800

    1260

---
+++ src/1260.二维网格迁移.cs
@@ -0,0 +1,16 @@
+/*
+ * @lc app=leetcode.cn id=1260 lang=csharp
+ *
+ * [1260] 二维网格迁移
+ */
+
+// @lc code=start
+public class Solution
+{
+    public IList<IList<int>> ShiftGrid(int[][] grid, int k)
+    {
+
+    }
+}
+// @lc code=end
+
