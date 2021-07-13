c54c5f97916ad84fc09dce1bd73c019d8120026c
ZWJ <330564052@qq.com>
2021-07-14 04:06:43 +0800

    1260

--- src/1260.二维网格迁移.cs
+++ src/1260.二维网格迁移.cs
@@ -5,11 +5,44 @@
  */
 
 // @lc code=start
+using System.Linq;
+using System.Collections.Generic;
 public class Solution
 {
     public IList<IList<int>> ShiftGrid(int[][] grid, int k)
     {
-
+        if (k == 0)
+        {
+            return grid;
+        }
+        var gridList = new List<int>();
+        var res = new List<List<int>>();
+        foreach (var gridItem in grid)
+        {
+            foreach (var item in gridItem)
+            {
+                gridList.Add(item);
+            }
+        }
+        var startIndex = gridList.Count - k;
+        bool finish = false;
+        var tempList = new List<int>();
+        var listCount = grid[0].Length;
+        for (int i = startIndex; i < gridList.Count; i++)
+        {
+            tempList.Add(gridList[i]);
+            if (tempList.Count == listCount)
+            {
+                res.Add(tempList);
+                
+            }
+            if (i == gridList.Count - 1 && finish == false)
+            {
+                i = 0;
+                finish = true;
+            }
+        }
+        return res;
     }
 }
 // @lc code=end
