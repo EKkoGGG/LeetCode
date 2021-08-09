7821585fdf62ff91028c9b8a2fe891406e7d4b19
ZWJ <330564052@qq.com>
2021-08-09 22:28:30 +0800

    0066

--- src/66.加一.java
+++ src/66.加一.java
@@ -1,3 +1,6 @@
+import java.util.ArrayList;
+import java.util.List;
+
 /*
  * @lc app=leetcode.cn id=66 lang=java
  *
@@ -7,7 +10,24 @@
 // @lc code=start
 class Solution {
     public int[] plusOne(int[] digits) {
-
+        for (int i = digits.length - 1; i >= 0; i--)
+        {
+            if (digits[i] == 9)
+            {
+                digits[i] = 0;
+            }
+            else
+            {
+                digits[i] += 1;
+                return digits;
+            }
+        }
+        int[] res = new int[digits.length + 1];
+        res[0] = 1;
+        for (int j = 1; j < res.length; j++) {
+            res[j] = 0;
+        }
+        return res;
     }
 }
 // @lc code=end
